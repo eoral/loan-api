7910b898a8bd3c13458ddd2117d70692f9a5ea75
Eray Oral <erayoral@gmail.com>
2025-07-26 21:56:01 +0300

    Remove test methods from controller

--- src/main/java/com/eoral/loanapi/controller/LoanController.java
+++ src/main/java/com/eoral/loanapi/controller/LoanController.java
@@ -1,11 +1,9 @@
 package com.eoral.loanapi.controller;
 
 import com.eoral.loanapi.dto.*;
-import com.eoral.loanapi.exception.BaseException;
 import com.eoral.loanapi.service.LoanService;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -30,19 +28,6 @@ public class LoanController {
 
     @PostMapping("{loanId}/payment")
     public PayLoanResponse payLoan(@PathVariable Long loanId, @RequestBody PayLoanRequest payLoanRequest) {
-        return loanService.payLoan(loanId, payLoanRequest);
-    }
-
-    @GetMapping("test1") // todo: remove
-    public LoanResponse test1() {
-        // Long customerId, BigDecimal amount, Integer numberOfInstallments, BigDecimal interestRate
-        CreateLoanRequest createLoanRequest = new CreateLoanRequest(1L, BigDecimal.valueOf(120000), 12, BigDecimal.valueOf(0.5));
-        return loanService.createLoan(createLoanRequest);
-    }
-
-    @GetMapping("test2") // todo: remove
-    public PayLoanResponse test2(@RequestParam Long loanId, @RequestParam BigDecimal amount) {
-        PayLoanRequest payLoanRequest = new PayLoanRequest(amount);
         return loanService.payLoan(loanId, payLoanRequest);
     }
 }
