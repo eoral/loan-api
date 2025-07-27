4c619e98093aad264e9e20f2600848f1b8b63878
Eray Oral <erayoral@gmail.com>
2025-07-28 00:11:04 +0300

    Add unit tests for DefaultLoanService

--- src/test/java/com/eoral/loanapi/service/impl/DefaultLoanServiceTest.java
+++ src/test/java/com/eoral/loanapi/service/impl/DefaultLoanServiceTest.java
@@ -1,23 +1,27 @@
 package com.eoral.loanapi.service.impl;
 
 import com.eoral.loanapi.dto.CreateLoanRequest;
+import com.eoral.loanapi.dto.LoanResponse;
 import com.eoral.loanapi.entity.Customer;
+import com.eoral.loanapi.entity.Loan;
+import com.eoral.loanapi.entity.LoanInstallment;
 import com.eoral.loanapi.repository.LoanInstallmentRepository;
 import com.eoral.loanapi.repository.LoanRepository;
 import com.eoral.loanapi.service.CustomerService;
 import com.eoral.loanapi.service.ValidationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static com.eoral.loanapi.TestUtils.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DefaultLoanServiceTest {
@@ -36,11 +40,15 @@ public class DefaultLoanServiceTest {
     private LoanInstallmentRepository loanInstallmentRepository;
     @InjectMocks
     private DefaultLoanService defaultLoanService;
+    @Captor
+    private ArgumentCaptor<Loan> loanCaptor;
+    @Captor
+    private ArgumentCaptor<List<LoanInstallment>> loanInstallmentsCaptor;
 
     private static final Long DEFAULT_CUSTOMER_ID = 1L;
-    private static final BigDecimal DEFAULT_CREDIT_LIMIT = BigDecimal.valueOf(100000);
+    private static final BigDecimal DEFAULT_CREDIT_LIMIT = BigDecimal.valueOf(100);
     private static final BigDecimal DEFAULT_USED_CREDIT_LIMIT = BigDecimal.ZERO;
-    private static final BigDecimal DEFAULT_AMOUNT_FOR_CREATE_LOAN = BigDecimal.valueOf(50000);
+    private static final BigDecimal DEFAULT_AMOUNT_FOR_CREATE_LOAN = BigDecimal.valueOf(60);
     private static final Integer DEFAULT_NUMBER_OF_INSTALLMENTS_FOR_CREATE_LOAN = 6;
     private static final BigDecimal DEFAULT_INTEREST_RATE_FOR_CREATE_LOAN = new BigDecimal("0.5");
 
@@ -52,19 +60,16 @@ public class DefaultLoanServiceTest {
         return customer;
     }
 
-    private static CreateLoanRequest newCreateLoanRequest() {
+    private static CreateLoanRequest newCreateLoanRequest(Customer customer) {
         return new CreateLoanRequest(
-                DEFAULT_CUSTOMER_ID,
+                customer.getId(),
                 DEFAULT_AMOUNT_FOR_CREATE_LOAN,
                 DEFAULT_NUMBER_OF_INSTALLMENTS_FOR_CREATE_LOAN,
                 DEFAULT_INTEREST_RATE_FOR_CREATE_LOAN);
     }
 
-    @Test
-    public void createLoanShouldSucceed() {
-        Customer customer = newCustomer();
+    private LoanResponse createLoan(Customer customer, CreateLoanRequest createLoanRequest) {
         BigDecimal availableCreditLimit = customer.getCreditLimit().subtract(customer.getUsedCreditLimit());
-        CreateLoanRequest createLoanRequest = newCreateLoanRequest();
         when(customerService.checkCustomer(createLoanRequest.customerId())).thenReturn(customer);
         doNothing().when(customerService).checkCustomerCanBeManagedByCurrentUser(customer);
         doNothing().when(validationService).checkAmountForCreateLoan(createLoanRequest.amount(), availableCreditLimit);
@@ -72,7 +77,57 @@ public class DefaultLoanServiceTest {
         doNothing().when(validationService).checkInterestRateForCreateLoan(createLoanRequest.interestRate());
         when(loanRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
         when(loanInstallmentRepository.saveAll(any())).thenAnswer(i -> i.getArguments()[0]);
-        doNothing().when(customerService).increaseUsedCreditLimit(any(), any());
-        defaultLoanService.createLoan(createLoanRequest);
+        doNothing().when(customerService).increaseUsedCreditLimit(customer, createLoanRequest.amount());
+        return defaultLoanService.createLoan(createLoanRequest);
+    }
+
+    @Test
+    public void createLoanShouldSucceed() {
+        Customer customer = newCustomer();
+        CreateLoanRequest createLoanRequest = newCreateLoanRequest(customer);
+        createLoan(customer, createLoanRequest);
+    }
+
+    @Test
+    public void createLoanShouldCreateLoanEntityWithExpectedValues() {
+        Customer customer = newCustomer();
+        CreateLoanRequest createLoanRequest = newCreateLoanRequest(customer);
+        createLoan(customer, createLoanRequest);
+        verify(loanRepository).save(loanCaptor.capture());
+        Loan loan = loanCaptor.getValue();
+        assertEquals(createLoanRequest.customerId(), loan.getCustomer().getId());
+        assertBigDecimalsAreEqual(createLoanRequest.amount(), loan.getLoanAmount());
+        assertEquals(createLoanRequest.numberOfInstallments(), loan.getNumberOfInstallments());
+        assertBigDecimalsAreEqual(createLoanRequest.interestRate(), loan.getInterestRate());
+        assertEquals(LocalDate.now(), loan.getStartDate()); // todo: is it correct
+        assertEquals(Boolean.FALSE, loan.getIsPaid());
+    }
+
+    @Test
+    public void createLoanShouldCreateLoanInstallmentEntityListWithCorrectSize() {
+        Customer customer = newCustomer();
+        CreateLoanRequest createLoanRequest = newCreateLoanRequest(customer);
+        createLoan(customer, createLoanRequest);
+        verify(loanInstallmentRepository).saveAll(loanInstallmentsCaptor.capture());
+        List<LoanInstallment> loanInstallments = loanInstallmentsCaptor.getValue();
+        assertEquals(createLoanRequest.numberOfInstallments(), loanInstallments.size());
+    }
+
+    @Test
+    public void createLoanShouldCreateLoanInstallmentEntityListWithExpectedValues() {
+        Customer customer = newCustomer();
+        CreateLoanRequest createLoanRequest = newCreateLoanRequest(customer);
+        createLoan(customer, createLoanRequest);
+        verify(loanInstallmentRepository).saveAll(loanInstallmentsCaptor.capture());
+        List<LoanInstallment> loanInstallments = loanInstallmentsCaptor.getValue();
+        for (LoanInstallment loanInstallment : loanInstallments) {
+            assertNotNull(loanInstallment.getLoan());
+            assertBigDecimalsAreEqual(BigDecimal.valueOf(10), loanInstallment.getAmountWithoutInterest());
+            assertBigDecimalsAreEqual(BigDecimal.valueOf(15), loanInstallment.getAmount());
+            assertNull(loanInstallment.getPaidAmount());
+            assertNotNull(loanInstallment.getDueDate());
+            assertNull(loanInstallment.getPaymentDate());
+            assertEquals(Boolean.FALSE, loanInstallment.getIsPaid());
+        }
     }
 }
